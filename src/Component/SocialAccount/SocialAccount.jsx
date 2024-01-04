027260de126fa37f68a4efa32daa03b8d635b6d6
devRokib <rakibahmedra971162@gmail.com>
2024-01-04 23:55:25 +0600

    first commit

---
+++ src/Component/SocialAccount/SocialAccount.jsx
@@ -0,0 +1,45 @@
+import './SocialAccount.css'
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth'
+import auth from '../../fireConfige/FirebaseConfig'
+import { NavLink, useNavigate } from 'react-router-dom'
+import Loading from '../Loading/Loading'
+import { toast } from 'react-toastify'
+
+
+function SocialAccount() {
+    const [signInWithGoogle,googleUser,googleLoading,gooleError] = useSignInWithGoogle(auth)
+    const [signInWithGithub,githubUser,githubLoading,githubError] = useSignInWithGithub(auth)
+    const navigate = useNavigate()
+    let errorElement;
+    if(googleLoading || githubLoading){
+        return <Loading/>
+    }
+    if(gooleError || githubError){
+        <p>
+            Error:{googleError?.message}
+            Error:{githubError?.message}
+        </p>
+    }
+    // if(googleUser || githubUser){
+    //     navigate('/');
+    //     toast.success('Log in Successfully')
+    // }
+  return (
+    <div>
+      <div className="buttonContainer">
+          <NavLink to='/' onClick={()=>signInWithGoogle()} className='signWithGoogle'>
+            <span className="GoogleContent">SignUp With </span>
+            <span className="GoogleIcon"><FcGoogle/></span>
+          </NavLink>
+          <NavLink to='/' onClick={()=>signInWithGithub()} className='signWithGithub'>
+            <span className="GithubContent">SignUp With </span>
+            <span className="GithubIcon"><FaGithub /></span>
+          </NavLink>
+          </div>
+    </div>
+  )
+}
+
+export default SocialAccount
---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import auth from '../fireConfige/FirebaseConfig'
+import { Navigate, useLocation } from 'react-router-dom'
+import Swal from 'sweetalert2'
+
+function PrivateRoute({ children }) {
+    const [user] = useAuthState(auth)
+    const location = useLocation()
+    
+    if(!user){
+        Swal.fire({
+            title:'Please at first Signup !',
+            icon:'warning'
+
+        })
+        return <Navigate to='/signup' state={{from:location}} replace/>
+    }else{
+     return (
+        <>
+            {children}
+        </>
+    )
+   }  
+}
+
+export default PrivateRoute;
